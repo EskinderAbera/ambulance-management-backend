24afe69aa0d9b42b36e75944f55b63b3670601f2
unknown <eske17.ea@gmail.com>
2022-10-16 01:06:49 +0300

    fixed user id issue

--- core/views.py
+++ core/views.py
@@ -115,6 +115,7 @@ class AssignMessage(APIView):
             driver = Driver.objects.get(id = request.data['id'])
         except Driver.DoesNotExist:
             return Response({"msg" : "driver does not exist!"}, status=status.HTTP_404_NOT_FOUND)
+        request.data['isactive'] = False
         serializer = DriverSerializer(driver, data=request.data)
         if serializer.is_valid():
             serializer.save()
